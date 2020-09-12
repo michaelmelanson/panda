32fcd9fdbd153b8e7b1a37275f590fe75a0a82e9
Michael Melanson <michael@michaelmelanson.net>
2020-09-12 15:02:03 -0400

    Print to serial when run

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -3,7 +3,7 @@ use core::fmt::Write;
 use lazy_static::lazy_static;
 use spin::Mutex;
 
-use crate::vga::Vga;
+use crate::{vga::Vga, serial_print};
 
 pub(crate) enum LogTarget {
     Vga(Vga),
@@ -11,6 +11,8 @@ pub(crate) enum LogTarget {
 
 impl Write for LogTarget {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        serial_print!("{}", s);
+        
         match self {
             LogTarget::Vga(vga) => vga.writer.write_str(s),
         }
